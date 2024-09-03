2c29c41632948b7059726f8f6a7c108e9a305988
0voFiona <fiona.nduu@strathmore.edu>
2024-09-03 12:51:45 +0300

    conditionals

---
+++ conditionals.js
@@ -0,0 +1,15 @@
+//CONDITIONALS
+
+//simple If/Else Statement
+const x = 30;
+
+
+
+if(x === 10 ){
+    console.log('x is 10');
+}else if(x > 10) {
+    console.log('x is greater than 10');
+}else {
+    console.log('x is less than 10');
+}
+

41fbda3bd13fe09995637aec0ec642df84455816
0voFiona <fiona.nduu@strathmore.edu>
2024-09-03 12:52:09 +0300

    hello

---
+++ hello.js
@@ -0,0 +1,25 @@
+console.log("Hello World");
+
+//variables - var, let, const345;
+let age = 30;
+
+
+let n = 123;
+n = 345;
+console.log(age);
+
+//const pie = 3.142; //assignment is a most
+//console.log(pie);
+
+
+const name = 'John'; //String
+const rating = 7.5; //Number
+const rate = "8.5"; //String
+const isAdmin = true; //Boolean
+const ans = null; //Null
+const v = undefined; //undefind
+let z; //undefined
+
+console.log(typeof name);
+console.log('My name is' +name+ 'and i am' +age);
+console.log('My name is ${name} and i am ${age}');
